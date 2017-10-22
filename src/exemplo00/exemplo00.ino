33b82ac3331de046efd01b36acb2f43169400cde
Walderlan Sena <senawalderlan@gmail.com>
2017-10-22 16:30:53 -0300

    Adicionado primeiro exemplo de codigo

---
+++ src/exemplo00/exemplo00.ino
@@ -0,0 +1,25 @@
+/**
+ * Mini Curso de Arduino (UNO)
+ * @author: Walderlan Sena <walderlan@mentesvirtuaissena.com>
+ * @github: https://github.com/WalderlanSena/miniCursoArduino
+ * @exemplo: 00 - Estrutura básica 
+ * @license: GPL License
+ */
+
+/**
+ * Método setup, onde se define a inicialização dos componentes
+ * e configurações gerais do software
+ */
+void setup() {
+
+
+}//end setup
+
+/**
+ * Método loop - onde se define as funcionalidade infinitas, onde sempre
+ * será repetido as instruções aqui definidas
+ */
+void loop() {
+
+
+}//end loop
