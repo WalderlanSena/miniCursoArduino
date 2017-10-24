b687bea12e44dbc4ebd121b771593a60c99c446e
Walderlan Sena <senawalderlan@gmail.com>
2017-10-23 22:38:48 -0300

    Adicionado exemplo 02 - Tipos Primitivos

---
+++ src/exemplo02/exemplo02.ino
@@ -0,0 +1,35 @@
+/**
+ * Mini Curso de Arduino (UNO)
+ * @author: Walderlan Sena <walderlan@mentesvirtuaissena.com>
+ * @github: https://github.com/WalderlanSena/miniCursoArduino
+ * @exemplo: 02 - Tipos Primitivos
+ * @license: GPL License
+ */
+//---------- Tipos Primitivos ----------
+/*
+    void          | boolean | char
+    unsigned char | byte    | word
+    unsigned int  | int     | long
+    unsigned long | short   | float
+    double
+    string - char array | String - object
+    array
+*/ 
+//Exemplo de algumas declarações
+int valor1    = 0;
+float valor2  = 0;
+bool valor6   = true;
+byte valor3   = 0;
+char valor4   = '0';
+String valor5 = "0";
+
+
+void setup () 
+{
+
+}//end setup
+
+void loop () 
+{
+
+}//end loop
